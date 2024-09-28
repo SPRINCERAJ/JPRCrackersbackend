f44ff388f2084a982e68cd3656b2e4296c8b9dbd
PRINCE RAJ S <148860531+SPRINCERAJ@users.noreply.github.com>
2024-09-28 22:51:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:'http://localhost:4200'
+}));
 app.use(bodyParser.json());
 
 // MongoDB connection

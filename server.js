ffee179d17fef569f4d5c28c7e2f23af1c5747b6
PRINCE RAJ S <148860531+SPRINCERAJ@users.noreply.github.com>
2024-09-28 23:06:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:'http://localhost:4200'
+  origin:'http://localhost:4200',
+  methods:['GET','POST'],
+  allowedHeaders:['Content-Type'],
 }));
 app.use(bodyParser.json());
 

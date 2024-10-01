f261d558f3f59db4f8a560f3e86dfc096a9396a2
PRINCE RAJ S <148860531+SPRINCERAJ@users.noreply.github.com>
2024-10-01 23:37:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const twilio = require('twilio');
+const twilio = require('twilio'); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
